8a7f52b3a429021a671e648e47a8bfb63c13c2b3
akosborn <andrewosborn93@gmail.com>
2017-01-22 23:27:15 -0600

    First commit

---
+++ src/main/java/com/umspreadsheet/UmspreadsheetApplication.java
@@ -0,0 +1,12 @@
+package com.umspreadsheet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UmspreadsheetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UmspreadsheetApplication.class, args);
+	}
+}
