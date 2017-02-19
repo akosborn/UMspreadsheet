78e246bdf3cbc1831b873314a59b83f751ba4c8a
akosborn <andrewosborn93@gmail.com>
2017-02-19 01:07:08 -0600

    Upgraded to Thymeleaf 3 with work-around found on spring-social issues page; removed Thymeleaf 2 CDATA tags from scripts; fixed review count bug on index

--- src/main/java/com/umspreadsheet/Application.java
+++ src/main/java/com/umspreadsheet/Application.java
@@ -2,10 +2,11 @@ package com.umspreadsheet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.social.SocialWebAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @EnableConfigurationProperties
-@SpringBootApplication
+@SpringBootApplication(exclude = SocialWebAutoConfiguration.class)
 public class Application
 {
 	public static void main(String[] args)
