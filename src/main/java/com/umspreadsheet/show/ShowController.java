c98a1defa23bd6480b5487f8f21f85f4e9724755
akosborn <andrewosborn93@gmail.com>
2017-04-19 01:01:01 -0500

    removed old clunky method from ShowController to map user reviews; removed success banners from shows.html

--- src/main/java/com/umspreadsheet/show/ShowController.java
+++ src/main/java/com/umspreadsheet/show/ShowController.java
@@ -71,7 +71,7 @@ public class ShowController
 
     // Returns a specified show's page
     @RequestMapping(value = "/shows/show", params = "showId", method = RequestMethod.GET)
-    public String showPage(@RequestParam(value = "showId") Long showId, Model model, Authentication authentication) throws
+    public String showPage(@RequestParam(value = "showId") Long showId, Model model) throws
             DataNotFoundException
     {
         String username = getCurrentUsername();
@@ -80,15 +80,6 @@ public class ShowController
         if (show == null)
             throw new DataNotFoundException("Show with id=" + showId + " not found.");
 
-        // List of the show's track IDs to be used for Javascript dynamic slider creation
-        Map<Long, Double> trackIdMap = new HashMap<>();
-
-        // Create a map for the show where the string represents a set
-        Map<String, Map<Track, TrackReviewForm>> trackAndReviewMap =
-                getUsersTracksAndReviews(username, show, trackIdMap);
-
-        model.addAttribute("trackIdMap", trackIdMap);
-        model.addAttribute("trackAndReviewMap", trackAndReviewMap);
         model.addAttribute("show", showService.findById(showId));
 
         SetDTO setDTO = new SetDTO();
@@ -98,10 +89,6 @@ public class ShowController
         Track track = new Track();
         track.setShowId(showId);
         model.addAttribute("track", track);
-
-        /**
-         * refactoring
-         */
 
         List<TrackReview> trackReviews = trackReviewService.findByUserAndShow(show, userService.findByUsername
                 (username));
@@ -127,14 +114,8 @@ public class ShowController
             }
         }
 
-        /**
-         *
-         */
         model.addAttribute("trackReviewForms", trackReviewForms);
         model.addAttribute("allReviews", trackReviewService.getAllByShow(showId));
-
-        if (!username.equals("anonymousUser"))
-            model.addAttribute("trackReviewForm", new TrackReviewForm(username));
 
         return "/show/show";
     }
@@ -233,52 +214,6 @@ public class ShowController
         }
 
         return username;
-    }
-
-    private Map<String, Map<Track, TrackReviewForm>> getUsersTracksAndReviews(String username, Show show,
-                                                                          Map<Long, Double> trackIdMap)
-    {
-        // Find the current user's reviews for the specified show
-        List<TrackReview> trackReviews = trackReviewService.findByUserAndShow
-                (show, userService.findByUsername(username));
-
-        Map<String, Map<Track, TrackReviewForm>> trackAndReviewMap = new LinkedHashMap<>();
-        // Loop through each set
-        for (Set set : show.getSets())
-        {
-            // Create a map for the set
-            Map<Track, TrackReviewForm> map = new LinkedHashMap<>();
-            // Add the set name(String) and the map of its tracks/reviews
-            trackAndReviewMap.put(set.getName(), map);
-
-            // Loop through all tracks in the set
-            for (Track track : set.getTracks())
-            {
-                // Set default score to null for Javascript slider dynamic setting
-                Double score = null;
-
-                // Create new trackReviewForm and set username via constructor
-                TrackReviewForm trackReviewForm = new TrackReviewForm(username, track.getId());
-
-                // Loop through user's reviews for this show
-                for (TrackReview trackReview : trackReviews)
-                {
-                    // Check if the user has reviewed the track
-                    if (track.getId().equals(trackReview.getTrack().getId()))
-                    {
-                        // If user has reviewed the track, populate a new trackReviewForm
-                        trackReviewForm = new TrackReviewForm(trackReview);
-                        score = trackReview.getScore();
-                        break;
-                    }
-                }
-
-                map.put(track, trackReviewForm);
-                trackIdMap.put(track.getId(), score);
-            }
-        }
-
-        return trackAndReviewMap;
     }
 
     private List<Show> setNumberOfReviews(List<com.umspreadsheet.show.Show> shows)
