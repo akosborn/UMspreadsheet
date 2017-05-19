ee16edf5480e4a40435e976eab169fb10726087d
akosborn <andrewosborn93@gmail.com>
2017-05-19 14:55:57 -0500

    fixed navbar profile url; updated UserController path for user profile

--- src/main/java/com/umspreadsheet/user/UserController.java
+++ src/main/java/com/umspreadsheet/user/UserController.java
@@ -3,6 +3,7 @@ package com.umspreadsheet.user;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.security.Principal;
@@ -19,11 +20,11 @@ public class UserController
         this.userService = userService;
     }
 
-    @RequestMapping("/profile")
-    public String getProfile(Model model, Principal principal)
+    @RequestMapping("/user/{username}")
+    public String getProfile(@PathVariable String username, Model model, Principal principal)
     {
-        User user = userService.findByUsername(principal.getName());
-        model.addAttribute("currentUser", new CurrentUser(user));
+        User user = userService.findByUsername(username);
+        model.addAttribute("user", user);
 
         return "user/profile";
     }
