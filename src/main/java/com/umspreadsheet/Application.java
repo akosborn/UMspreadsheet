5b0fbdce1cc724d532566ab370e378e761d45fad
akosborn <andrewosborn93@gmail.com>
2017-05-23 22:27:05 -0500

    changed packaging to war; set Tomcat dependency to provided; added SpringBootServletInitializer to Application.class

--- src/main/java/com/umspreadsheet/Application.java
+++ src/main/java/com/umspreadsheet/Application.java
@@ -3,15 +3,17 @@ package com.umspreadsheet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.social.SocialWebAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 
 @EnableConfigurationProperties
 @SpringBootApplication(exclude = SocialWebAutoConfiguration.class)
-public class Application
+public class Application extends SpringBootServletInitializer
 {
 	@Bean
 	public EmbeddedServletContainerCustomizer containerCustomizer()
@@ -21,6 +23,12 @@ public class Application
 			ErrorPage custom404Page = new ErrorPage(HttpStatus.NOT_FOUND,"/404");
 			container.addErrorPages(custom404Page);
 		});
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(Application.class);
 	}
 
 	public static void main(String[] args)
