1f455fa1a5083f8301b00336452c8063f95ee7b4
akosborn <andrewosborn93@gmail.com>
2017-05-24 22:11:42 -0500

    setup environment for localhost

--- src/main/java/com/umspreadsheet/Application.java
+++ src/main/java/com/umspreadsheet/Application.java
@@ -3,17 +3,15 @@ package com.umspreadsheet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.social.SocialWebAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ErrorPage;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 
 @EnableConfigurationProperties
 @SpringBootApplication(exclude = SocialWebAutoConfiguration.class)
-public class Application extends SpringBootServletInitializer
+public class Application
 {
 	@Bean
 	public EmbeddedServletContainerCustomizer containerCustomizer()
@@ -23,12 +21,6 @@ public class Application extends SpringBootServletInitializer
 			ErrorPage custom404Page = new ErrorPage(HttpStatus.NOT_FOUND,"/404");
 			container.addErrorPages(custom404Page);
 		});
-	}
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
-	{
-		return application.sources(Application.class);
 	}
 
 	public static void main(String[] args)
