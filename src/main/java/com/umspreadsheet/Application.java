5f041f42574ecd379b00dab9a6f3211d91a8147a
akosborn <andrewosborn93@gmail.com>
2017-05-27 21:15:27 -0500

    extended SpringBootServletInitializer in Application.class

--- src/main/java/com/umspreadsheet/Application.java
+++ src/main/java/com/umspreadsheet/Application.java
@@ -3,15 +3,40 @@ package com.umspreadsheet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.social.SocialWebAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
+/*
+* Configuration for dev environment
+* */
+//@EnableConfigurationProperties
+//@SpringBootApplication(exclude = SocialWebAutoConfiguration.class)
+//public class Application
+//{
+//	@Bean
+//	public EmbeddedServletContainerCustomizer containerCustomizer()
+//	{
+//		return ( container ->
+//		{
+//			ErrorPage custom404Page = new ErrorPage(HttpStatus.NOT_FOUND,"/404");
+//			container.addErrorPages(custom404Page);
+//		});
+//	}
+//
+//	public static void main(String[] args)
+//	{
+//		SpringApplication.run(Application.class, args);
+//	}
+//}
 
+// Configuration for war
 @EnableConfigurationProperties
 @SpringBootApplication(exclude = SocialWebAutoConfiguration.class)
-public class Application
+public class Application extends SpringBootServletInitializer
 {
 	@Bean
 	public EmbeddedServletContainerCustomizer containerCustomizer()
@@ -21,6 +46,12 @@ public class Application
 			ErrorPage custom404Page = new ErrorPage(HttpStatus.NOT_FOUND,"/404");
 			container.addErrorPages(custom404Page);
 		});
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(Application.class);
 	}
 
 	public static void main(String[] args)
