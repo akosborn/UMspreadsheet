1503ffe38701734410a3f647886035aa56accddc
akosborn <andrewosborn93@gmail.com>
2018-09-30 02:08:40 -0400

    Removed use of JsonReader in HomeCOntroller

--- src/main/java/com/umspreadsheet/home/HomeController.java
+++ src/main/java/com/umspreadsheet/home/HomeController.java
@@ -1,8 +1,6 @@
 package com.umspreadsheet.home;
 
-import com.umspreadsheet.parse.JsonReader;
 import com.umspreadsheet.set.Set;
-import com.umspreadsheet.set.SetService;
 import com.umspreadsheet.show.Show;
 import com.umspreadsheet.show.ShowService;
 import com.umspreadsheet.track.Track;
@@ -12,7 +10,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.FileNotFoundException;
 import java.util.List;
 
 @Controller
@@ -20,26 +17,17 @@ public class HomeController
 {
     private TrackService trackService;
     private ShowService showService;
-    private SetService setService;
 
     @Autowired
-    public HomeController(TrackService trackService, ShowService showService, SetService setService)
+    public HomeController(TrackService trackService, ShowService showService)
     {
         this.trackService = trackService;
         this.showService = showService;
-        this.setService = setService;
     }
 
     @RequestMapping("/")
     public String home(Model model)
     {
-        JsonReader reader = new JsonReader(showService, setService, trackService);
-        try {
-            reader.readFromJson();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-
         model.addAttribute("topTenSongs", trackService.findTopTen());
         model.addAttribute("topTenShows", showService.findTopTenShows());
         model.addAttribute("lastThreeShows", setNumberOfReviews(showService.getLastThreeShows()));
